d55096a1ca12fce9669edf3bd148ef141f2e4ad8
Tony Tin Nguyen <nguyenquangtin@gmail.com>
2026-03-17 14:49:53 +0700

    fix: Update Vercel Analytics import to /next path

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion'
 import Chakra from '../components/chakra'
 import Payhip from '../components/payhip'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 
 if (typeof window !== 'undefined') {
   window.history.scrollRestoration = 'manual'
